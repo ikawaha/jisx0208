e71fd3ee6e31550ad5007fb4f05ca2926b21ed0f
ikawaha <ikawaha@users.noreply.github.com>
2022-08-28 23:17:11 +0900

    feat: Spport input from stdin

--- cmd/jisx0208/main.go
+++ cmd/jisx0208/main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 
 	"github.com/ikawaha/jisx0208"
 )
 
+func usage() {
+	fmt.Fprintf(os.Stderr, "jisx0208 [target_string]\n")
+}
+
 func main() {
-	if err := run(os.Args); err != nil {
+	if err := run(os.Args[1:]); err != nil {
 		fmt.Fprintf(os.Stderr, "%v", err)
 		os.Exit(1)
 	}
 }
 
 func run(args []string) error {
-	if len(args) < 1 {
-		return fmt.Errorf("usage: jisx0208 <string>")
+	if len(args) >= 1 {
+		fmt.Println(jisx0208.ToValid(args[0], "□"))
+		return nil
 	}
-	fmt.Println(jisx0208.ToValid(args[1], "□"))
-	return nil
+	fp := os.Stdin
+	s := bufio.NewScanner(fp)
+	for s.Scan() {
+		fmt.Println(jisx0208.ToValid(s.Text(), "□"))
+	}
+	return s.Err()
 }
